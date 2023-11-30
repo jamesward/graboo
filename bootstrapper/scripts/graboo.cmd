780e0d940cc7dc238fdc51b2166556da1721886c
James Ward <james@jamesward.com>
2023-11-29 23:42:22 -0700

    typooo

--- bootstrapper/scripts/graboo.cmd
+++ bootstrapper/scripts/graboo.cmd
@@ -4,7 +4,7 @@ if exist "%USERPROFILE%\graboo-windows-x64.exe" goto runGraboo
 
 powershell -Command "&{"^
 		"$webclient = new-object System.Net.WebClient;"^
-		"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $webclient.DownloadFile('https://github.com/jamesward/graboo/releases/latest/download/graboo-windown-x64.exe', '%USERPROFILE%\graboo-windows-x64.exe')"^
+		"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $webclient.DownloadFile('https://github.com/jamesward/graboo/releases/latest/download/graboo-windows-x64.exe', '%USERPROFILE%\graboo-windows-x64.exe')"^
 		"}"
 
 :runGraboo
