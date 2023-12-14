7d1d61e74b3302dc1aa525b82b9abc2335aa69ec
James Ward <james@jamesward.com>
2023-12-13 22:48:52 -0700

    hmmm

--- templater/src/macosMain/kotlin/Templater.macos.kt
+++ templater/src/macosMain/kotlin/Templater.macos.kt
@@ -3,6 +3,6 @@ import okio.Path
 import platform.posix.chmod
 
 actual fun makeExecutable(path: Path) {
-    val mode = 0b111_111_111u
+    val mode: UShort = 0b111_111_111u
     chmod(path.toString(), mode)
 }
